bb27bafcdd37e480631a04183007cb62472e8494
rui-watanabe <wataru610806@gmail.com>
2020-04-24 19:23:21 +0900

    return products.json by using getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,27 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 
-function Home() {
-  useEffect(() => {
-    getProducts()
-  },[])
+function Home({ products }) {
+  console.log(products);
+  // useEffect(() => {
+  //   getProducts()
+  // },[])
 
-  async function getProducts(){
-    const url = 'http://localhost:3000/api/products'
-    const response = await axios.get(url);
-    console.log(response.data);
-  }
+  // async function getProducts(){
+  //   const url = 'http://localhost:3000/api/products'
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // }
 
   return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+//fetch data on server
+  const url = 'http://localhost:3000/api/products'
+  const response = await axios.get(url);
+  return { products: response.data };
+//return response data as an object(this object will be merged with existing props )
 }
 
 export default Home;
