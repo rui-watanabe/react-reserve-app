b04d8e0bb3974aa778d1e681457b460f5dbed5e6
rui-watanabe <wataru610806@gmail.com>
2020-04-26 14:42:17 +0900

    build product card

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
 
 
 function Home({ products }) {
-  console.log(products);
-  // useEffect(() => {
-  //   getProducts()
-  // },[])
-
-  // async function getProducts(){
-  //   const url = 'http://localhost:3000/api/products'
-  //   const response = await axios.get(url);
-  //   console.log(response.data);
-  // }
-
-  return <>home</>;
+  return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async () => {
