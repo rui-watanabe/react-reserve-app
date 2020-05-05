4aafe9bf203abc37323f1cbcba6797e8a1b0eb9b
rui-watanabe <wataru610806@gmail.com>
2020-05-05 14:02:19 +0900

    catch errors on cliant and server

--- pages/create.js
+++ pages/create.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Form, Input, TextArea, Button, Image, Message, Header, Icon } from 'semantic-ui-react';
 import axios from 'axios';
 import baseUrl from '../utils/baseUrl';
+import catchErrors from '../utils/catchErrors';
 
 const INITIAL_PRODUCT = {
   name: "",
@@ -16,6 +17,7 @@ function CreateProduct() {
   const [success, setSuccess] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [disabled, setDisabled] = React.useState(true);
+  const [error, setError] = React.useState('');
 
   React.useEffect(() => {
     const isProduct = Object.values(product).every(el => Boolean(el));
@@ -43,17 +45,23 @@ function CreateProduct() {
   }
 
   async function handleSubmit(event){
-    event.preventDefault();
-    setLoading(true);
-    const mediaUrl = await handleImageUpload();
-    const url = `${baseUrl}/api/product`;
-    const { name, price, description } = product;
-    const payload = { name, price ,description, mediaUrl};
-    const response = await axios.post(url, payload);
-    console.log({response});
-    setLoading(false);
-    setProduct(INITIAL_PRODUCT);
-    setSuccess(true);
+    try{
+      event.preventDefault();
+      setLoading(true);
+      const mediaUrl = await handleImageUpload();
+      const url = `${baseUrl}/api/product`;
+      const { name, price, description } = product;
+      const payload = { name, price ,description, mediaUrl};
+      const response = await axios.post(url, payload);
+      console.log({response});
+      setProduct(INITIAL_PRODUCT);
+      setSuccess(true);
+    } catch(error){
+      catchErrors(error, setError);
+    } finally {
+      setLoading(false);
+    }
+    
   }
   
 
@@ -63,7 +71,12 @@ function CreateProduct() {
         <Icon name="add" color="orange" />
         Create New Product
       </Header>
-      <Form loading={loading} success={success} onSubmit={handleSubmit}>
+      <Form loading={loading} error={Boolean(error)} success={success} onSubmit={handleSubmit}>
+        <Message
+          error
+          header="Oops!"
+          content={error}
+        />
         <Message
           success
           icon="check"
