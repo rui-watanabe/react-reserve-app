deee7af68e5ced872de718220863f0d047675099
rui-watanabe <wataru610806@gmail.com>
2020-05-07 22:20:33 +0900

    create ptotected route redirect user

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
 import App from "next/app";
-import Layout from "../components/_App/Layout"
+import Layout from "../components/_App/Layout";
+import { parseCookies } from 'nookies';
+import { redirectUser } from '../utils/auth';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }){
 //各コンポーネント.getInitialPropsで指定したオブジェクトのデータがctxに入る
+    const { token } = parseCookies(ctx);
+
     let pageProps = {};
 
 //非同期でサーバーにレンダリング
     if(Component.getInitialProps){
       pageProps = await Component.getInitialProps(ctx);
+    }
+
+    if(!token){
+      const isProtectedRoute = 
+        ctx.pathname === '/account' || ctx.pathname === '/create';
+      if(isProtectedRoute){
+        redirectUser(ctx, '/login');
+      }
     }
 
 //this is an object and return props
