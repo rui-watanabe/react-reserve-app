f146de41dba56e5fe6ca03c9b27bcb8e499d2253
rui-watanabe <wataru610806@gmail.com>
2020-05-09 22:53:27 +0900

    update cart information function

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Input } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import baseUrl from '../../utils/baseUrl';
+import cookie from 'js-cookie';
 
-function AddProductToCart() {
+function AddProductToCart({ user, productId }) {
   const [quantity, setQuantity] = React.useState(1);
+  const router = useRouter();
+
+  async function handleAddProductToCart(){
+    const url = `${baseUrl}/api/cart`;
+    const payload = { quantity, productId};
+    const token = cookie.get('token');
+    const headers = { headers: { Authorization: token } };
+    await axios.put(url, payload, headers);
+  }
+
   return(
     <Input
       type="number"
@@ -10,10 +24,16 @@ function AddProductToCart() {
       placeholder="Quantity"
       value={quantity}
       onChange={event => setQuantity(Number(event.target.value))}
-      action={{
+      action={user ? {
         color: "orange",
         content: "Add to Cart",
-        icon: "plus cart"
+        icon: "plus cart",
+        onClick: handleAddProductToCart
+      } : {
+        color: "blue",
+        content: "Sign Up To Purchase",
+        icon: "signup",
+        onClick: () => router.push('/signup')
       }}
     />
   )
