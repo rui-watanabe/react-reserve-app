df9c4db453840023dab941e8247817f1be8e4dd0
rui-watanabe <wataru610806@gmail.com>
2020-05-16 14:33:55 +0900

    get orders data

--- pages/account.js
+++ pages/account.js
@@ -1,13 +1,28 @@
 import AccountHeader from '../components/Account/AccountHeader';
 import AccountOrders from '../components/Account/AccountOrders';
+import { parseCookies } from 'nookies';
+import baseUrl from '../utils/baseUrl';
+import axios from 'axios';
 
-function Account({ user }) {
+function Account({ user, orders }) {
+  console.log(orders);
   return(
     <>
       <AccountHeader { ...user }/>
       <AccountOrders/>
     </>
   )
+}
+
+Account.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx);
+  if(!token){
+    return { orders: [] };
+  }
+  const payload = { headers: { Authorization: token } };
+  const url = `${baseUrl}/api/orders`;
+  const response = await axios.get(url, payload);
+  return response.data;
 }
 
 export default Account;
