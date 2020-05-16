aed8676ecf1f45d50ef2e5a74b0e84ae241176b8
rui-watanabe <wataru610806@gmail.com>
2020-05-16 16:16:58 +0900

    get users data

--- components/Account/AccountPermissions.js
+++ components/Account/AccountPermissions.js
@@ -1,5 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+import baseUrl from '../../utils/baseUrl';
+import cookie from 'js-cookie';
+
 function AccountPermissions() {
-  return <>AccountPermissions</>;
+  const [users, setUsers] = React.useState([])
+
+  React.useEffect(() => {
+    getUsers();
+  },[]);
+
+  async function getUsers(){
+    const url = `${baseUrl}/api/users`;
+    const token = cookie.get('token');
+    const payload = { headers: { Authorization: token } };
+    const response = await axios.get(url, payload);
+    console.log(response.data)
+    // setUsers(response.data);
+  }
+
+  return(
+    <>
+    </>
+  )
 }
 
 export default AccountPermissions;
