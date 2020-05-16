11de0c1bbe71f1fd08e7599daae2e24620b0f645
rui-watanabe <wataru610806@gmail.com>
2020-05-16 19:53:23 +0900

    sort oreders and userpermission

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -8,6 +8,7 @@ export default async (req, res) => {
   try{
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
     const orders = await Order.find({ user: userId })
+      .sort({ createdAt: "desc"})
       .populate({
         path: "products.product",
         model: "Product"
--- pages/api/users.js
+++ pages/api/users.js
@@ -5,7 +5,7 @@ export default async (req,res) => {
   try{
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
     //$ne = not equal
-    const users = await User.find({ _id: { $ne: userId } });
+    const users = await User.find({ _id: { $ne: userId } }).sort({ role: "ascgit " });
     res.status(200).json(users);
   }catch(errors){
     console.error(errors);
