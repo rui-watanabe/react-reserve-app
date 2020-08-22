27375909cfa52a665dba38fa1a39261ac219f081
rui-watanabe <wataru610806@gmail.com>
2020-08-22 16:21:40 +0900

    ProductPagination ts set

--- components/Index/ProductPagination.tsx
+++ components/Index/ProductPagination.tsx
@@ -1,19 +1,22 @@
-import  { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import { Container, Pagination } from 'semantic-ui-react';
+import { ProductPageNationProps } from './ProductType';
 
-function ProductPagination({ totalPages }) {
+function ProductPagination({ totalPages }: ProductPageNationProps) {
   const router = useRouter();
-  return(
+  return (
     <Container textAlign="center" style={{ margin: '2em' }}>
-    <Pagination
-      defaultActivePage={1}
-      totalPages={totalPages}
-      onPageChange={(event, data) => {
-        data.activePage === 1 ? router.push("/") : router.push(`/?page=${data.activePage}`);
-      }}
-    />
+      <Pagination
+        defaultActivePage={1}
+        totalPages={totalPages}
+        onPageChange={(event, data) => {
+          data.activePage === 1
+            ? router.push('/')
+            : router.push(`/?page=${data.activePage}`);
+        }}
+      />
     </Container>
-  )
+  );
 }
 
 export default ProductPagination;
--- components/Index/ProductType.ts
+++ components/Index/ProductType.ts
@@ -3,3 +3,7 @@ import { ProductModelType } from '../../models/ProductModel/ProductType';
 export interface ProductListProps {
   products: ProductModelType[];
 }
+
+export interface ProductPageNationProps {
+  totalPages: number;
+}
