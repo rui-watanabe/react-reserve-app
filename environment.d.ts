f4e3016b3bea81c75440be46778da5bae9cdeeb7
rui-watanabe <wataru610806@gmail.com>
2020-08-30 22:37:48 +0900

    fix1 enviroment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,8 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      JWT_SECRET: string;
-      CLOUDINARY_URL: string;
-      STRIPE_SECRET_KEY: string;
-      STRIPE_PUBLIC_KEY: string;
-    }
+namespace NodeJS {
+  interface ProcessEnv {
+    JWT_SECRET: string;
+    CLOUDINARY_URL: string;
+    STRIPE_SECRET_KEY: string;
+    STRIPE_PUBLIC_KEY: string;
   }
 }
