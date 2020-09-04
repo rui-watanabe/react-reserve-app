78c1886119591482e3002596f96619cd62b417b3
rui-watanabe <wataru610806@gmail.com>
2020-09-04 22:34:17 +0900

    product.ts ts set

--- pages/product.tsx
+++ pages/product.tsx
@@ -2,8 +2,9 @@ import axios from 'axios'
 import ProductSummary from '../components/Product/ProductSummary'
 import ProductAttributes from '../components/Product/ProductAttributes'
 import baseUrl from '../utils/baseUrl'
+import { ProductProps, ProductInitialProps } from '../pagesType/ProductType'
 
-function Product({ product, user }) {
+function Product({ product, user }: ProductProps) {
   return (
     <>
       <ProductSummary user={user} {...product} />
@@ -12,7 +13,7 @@ function Product({ product, user }) {
   )
 }
 
-Product.getInitialProps = async ({ query: { _id } }) => {
+Product.getInitialProps = async ({ query: { _id } }: ProductInitialProps) => {
   const url = `${baseUrl}/api/product`
   const payload = { params: { _id } }
   const response = await axios.get(url, payload)
---
+++ pagesType/ProductType.ts
@@ -0,0 +1,13 @@
+import { ProductModelType } from "../models/ProductModel/ProductType";
+import { UserModelType } from "../models/UserModel/UserType";
+
+export interface ProductProps {
+  product: ProductModelType;
+  user: UserModelType;
+}
+
+export interface ProductInitialProps {
+  query: {
+    _id: string
+  }
+}
