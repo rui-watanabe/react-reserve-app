2410590f19339a4f47cb96bb3ed2c54cb216464a
rui-watanabe <wataru610806@gmail.com>
2020-09-05 14:19:38 +0900

    deploy vercel

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,3 +1,6 @@
-const baseUrl = 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://mern-with-typescript-reserve-app.now.sh'
+    : 'http://localhost:3000';
 
 export default baseUrl;
