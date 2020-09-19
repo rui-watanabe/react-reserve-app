ddf1d6694c17b929fc051f6288470ff1ace69b26
rui-watanabe <wataru610806@gmail.com>
2020-09-19 17:52:09 +0900

    create snapshot of ProductList.spec.tsx

---
+++ src/components/__tests__/Index/ProductList.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { testProductList } from '../../../../static/testProductList';
+import ProductList from '../../Index/ProductList';
+
+test('ProductList', () => {
+  const component = renderer.create(<ProductList products={testProductList} />);
+  const tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
