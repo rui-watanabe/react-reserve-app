6375e022fbf433620719db1a82d3572db00f7d79
rui-watanabe <wataru610806@gmail.com>
2020-09-21 13:52:02 +0900

    create snapshot of Layout.spec.tsx

---
+++ src/components/__tests__/_App/Layout.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Layout from '../../_App/Layout';
+import { testUser } from '../../../../static/testData';
+
+jest.mock('next/router', () => ({
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+    };
+  },
+}));
+test('Layout', () => {
+  const component = renderer.create(<Layout user={testUser} />);
+  const tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
