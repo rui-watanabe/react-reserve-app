01997abdcf2d069a08de84577eeecc692882a3e3
rui-watanabe <wataru610806@gmail.com>
2020-09-21 13:24:40 +0900

    fix1 snapshot of AccountPermissions.spec.tsx

--- src/components/Account/AccountPermissions.tsx
+++ src/components/Account/AccountPermissions.tsx
@@ -44,10 +44,12 @@ function AccountPermissions(): JSX.Element {
           </Table.Row>
         </Table.Header>
 
-        <Table.Body>
-          {users.map((user) => (
-            <UserPermission key={String(user._id)} user={user} />
-          ))}
+        <Table.Body data-testid="user-permission">
+          {users.length != 0
+            ? users.map((user) => (
+                <UserPermission key={String(user._id)} user={user} />
+              ))
+            : null}
         </Table.Body>
       </Table>
     </div>
--- src/components/__tests__/Account/AccountPermissions.spec.tsx
+++ src/components/__tests__/Account/AccountPermissions.spec.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { testUser } from '../../../../static/testData';
 import { testUserList } from '../../../../static/testUserList';
 import AccountPermissions from '../../Account/AccountPermissions';
-import axios from 'axios';
 
-jest.mock('axios', () => {
-  return {
-    get: jest.fn(() => {
-      Promise.resolve({ data: { ...testUserList } });
-    }),
-  };
-});
+const realUseState = React.useState;
 
-test('AccountPermissions', async () => {
-  const response = await axios.get('');
-  console.log(response);
+jest
+  .spyOn(React, 'useState')
+  .mockImplementationOnce(
+    () => realUseState(testUserList) as [unknown, React.Dispatch<unknown>],
+  );
+
+test('AccountPermissions', () => {
   const component = renderer.create(<AccountPermissions />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
