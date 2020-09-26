a4872685e641671c632db6c68974f917a1420638
rui-watanabe <wataru610806@gmail.com>
2020-09-26 17:25:00 +0900

    testing take in products from faker

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,6 +1,9 @@
 import Product from '../../../models/ProductModel/Product';
 import connectDB from '../../../utils/connectDb';
 import { NextApiRequest, NextApiResponse } from 'next';
+// import faker from 'faker';
+// import { ProductModelType } from '../../../models/Types/ProductType';
+// import { ObjectId } from 'mongodb';
 
 connectDB();
 
@@ -21,6 +24,29 @@ export default async (
     const skips = pageSize * (pageNum - 1);
     products = await Product.find().skip(skips).limit(pageSize);
   }
+
+  // take in products from faker
+  // if (products.length > 0 && products.length !== pageSize) {
+  //   products = await createFakeData(products);
+  // }
+  // products = createFakeData(products);
+  // const totalPages = Math.ceil(totalDocs / pageSize);
   // const products = await Product.find();
   res.status(200).json({ products, totalPages });
 };
+
+// function createFakeData(products: ProductModelType[]) {
+//   let i;
+//   for (i = products.length; i < 9; ++i) {
+//     products.push({
+//       _id: new ObjectId('000000000000000000000000'),
+//       name: faker.commerce.productName(),
+//       price: Number(faker.commerce.price()),
+//       sku: faker.random.uuid(),
+//       description: faker.commerce.productDescription(),
+//       mediaUrl: faker.image.imageUrl(),
+//     });
+//   }
+//   console.log(products);
+//   return products;
+// }
