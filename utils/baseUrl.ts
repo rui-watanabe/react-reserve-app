94d5da48dceb34cb41eb4b2c69b00194bb6f879c
rui-watanabe <wataru610806@gmail.com>
2020-09-27 16:14:14 +0900

    fix can not input of create product form

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,6 +1,3 @@
-const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://mern-with-typescript-reserve-app.now.sh'
-    : 'http://localhost:3000';
+const baseUrl = 'http://localhost:3000';
 
 export default baseUrl;
