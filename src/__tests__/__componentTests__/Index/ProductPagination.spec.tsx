c655074ce90ffa1d6d42446fbba94a1126eaf319
rui-watanabe <wataru610806@gmail.com>
2020-10-03 23:04:48 +0900

    add useroute mock of ProductPagenation.spec.tsx

--- src/__tests__/__componentTests__/Index/ProductPagination.spec.tsx
+++ src/__tests__/__componentTests__/Index/ProductPagination.spec.tsx
@@ -3,6 +3,21 @@ import renderer from 'react-test-renderer';
 import { testProductList } from '../../../../static/testProductList';
 import ProductPagination from '../../../components/Index/ProductPagination';
 
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+    };
+  },
+}));
 test('ProductPagination', () => {
   const component = renderer.create(
     <ProductPagination totalPages={Math.floor(testProductList.length / 9)} />,
