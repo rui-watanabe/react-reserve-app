b3f5df268d896c9328835fb1fe646cfc511178a9
rui-watanabe <wataru610806@gmail.com>
2020-10-06 22:07:01 +0900

    add function test of caluculateCartTotal.ts

--- src/__tests__/__utilsTests__/calculateCartTotal.spec.ts
+++ src/__tests__/__utilsTests__/calculateCartTotal.spec.ts
@@ -1,12 +1,12 @@
-import { testCartList } from '../../static/testCartList';
-import calculateCartTotal from '../calculateCartTotal';
+import { testCartList } from '../../../static/testCartList';
+import calculateCartTotal from '../../../utils/calculateCartTotal';
 
 describe('calculateCartTotal.ts Functions TestCases', () => {
   it('should return the calculated value', () => {
     const result = calculateCartTotal(testCartList);
     const expected = {
-      cartTotal: 1,
-      stripeTotal: 1,
+      cartTotal: 4598.88,
+      stripeTotal: 459888,
     };
 
     expect(result).toStrictEqual(expected);
