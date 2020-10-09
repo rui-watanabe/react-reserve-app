cfdb9297445267da813dc26bad45f20bb5403c74
rui-watanabe <wataru610806@gmail.com>
2020-10-09 22:59:14 +0900

    add Logined snapshot of Login.spec.tsx

--- src/__tests__/__pagesTests__/Login.spec.tsx
+++ src/__tests__/__pagesTests__/Login.spec.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { testUser } from '../../../static/testData';
 import Login from '../../pages/login';
-
 test('Login', () => {
+  const component = renderer.create(<Login />);
+  const tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+const realUseState = React.useState;
+
+jest
+  .spyOn(React, 'useState')
+  .mockImplementationOnce(
+    () => realUseState(testUser) as [unknown, React.Dispatch<unknown>],
+  );
+test('Logined', () => {
   const component = renderer.create(<Login />);
   const tree = component.toJSON();
 
