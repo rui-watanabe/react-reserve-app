fc5594362829bed859defc168f5ee9f2e4f5170f
rui-watanabe <wataru610806@gmail.com>
2020-10-10 18:38:08 +0900

    add Signuped snapshot of Signup.spec.tsx

--- src/__tests__/__pagesTests__/Login.spec.tsx
+++ src/__tests__/__pagesTests__/Login.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { testUser } from '../../../static/testData';
 import Login from '../../pages/login';
+
 test('Login', () => {
   const component = renderer.create(<Login />);
   const tree = component.toJSON();
--- src/__tests__/__pagesTests__/Signup.spec.tsx
+++ src/__tests__/__pagesTests__/Signup.spec.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { testUser } from '../../../static/testData';
 import Signup from '../../pages/signup';
 
+test('Signup', () => {
+  const component = renderer.create(<Signup />);
+  const tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+const realUseState = React.useState;
+
+jest
+  .spyOn(React, 'useState')
+  .mockImplementationOnce(
+    () => realUseState(testUser) as [unknown, React.Dispatch<unknown>],
+  );
 test('Signup', () => {
   const component = renderer.create(<Signup />);
   const tree = component.toJSON();
